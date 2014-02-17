3dd07628fc0f60c041d271d610974fef2028f4c0
Kapa <kapa@sipaksi.net>
2014-02-17 16:53:37 +0100

    Some updates.

--- xsd.js
+++ xsd.js
@@ -1,7 +1,7 @@
-define(['Xml'], function () {
-	var Xsd = {
+define(['xml'], function () {
+	var xsd = {
 		xs: 'http://www.w3.org/2001/XMLSchema',
-		xsi: 'http://www.w3.org/2001/XMLSchema-instance',
+		xsi: 'http://www.w3.org/2001/XMLSchema-instance'
 	};
-	return Xsd;
+	return xsd;
 });
